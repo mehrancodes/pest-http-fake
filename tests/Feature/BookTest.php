ecf9bf5d82b61c8655713f538c3395d235e36211
Mehran Rasulian <7046255+mehrancodes@users.noreply.github.com>
2022-06-28 15:16:28 +0300

    Added Books API test

---
+++ tests/Feature/BookTest.php
@@ -0,0 +1,53 @@
+<?php
+
+use Illuminate\Support\Facades\Http;
+
+it('can get a list of books from the API', function () {
+    $responseData = fixture('BooksApi/book-list');
+
+    Http::fake([
+        '*' => Http::response(
+            body: $responseData
+        ),
+    ]);
+
+    $books = Http::get('https://books-api.com/books');
+
+    expect($books->json())->toEqual($responseData);
+
+    collect($books->json('data'))->each(function ($book) {
+        expect($book['author'])->toEqual('J R R Tolkien');
+    });
+});
+
+it('can create a new book', function () {
+    $responseData = fixture('BooksApi/create-book');
+
+    Http::fake([
+        '*' => Http::response(
+            body: $responseData,
+            status: 201,
+        ),
+    ]);
+
+    $book = Http::post('https://books-api.com/books', [
+        'title' => 'Spock Must Die!',
+        'author' => 'James Blish',
+    ]);
+
+    expect($book->json())->toEqual($responseData)
+        ->and($book->json('data'))->title->toEqual('Spock Must Die!');
+});
+
+it('can delete a book from the API', function () {
+    Http::fake([
+        '*' => Http::response(
+            body: null,
+            status: 204,
+        ),
+    ]);
+
+    $response = Http::delete('https://books-api.com/books/12345');
+
+    expect($response->status())->toEqual(204);
+});
--- tests/Pest.php
+++ tests/Pest.php
@@ -39,7 +39,20 @@ expect()->extend('toBeOne', function () {
 |
 */
 
-function something()
+function fixture(string $name): array
 {
-    // ..
+    $file = file_get_contents(
+        filename: base_path("tests/Fixtures/$name.json"),
+    );
+
+    if(! $file) {
+        throw new InvalidArgumentException(
+            message: "Cannot find fixture: [$name] at tests/Fixtures/$name.json",
+        );
+    }
+
+    return json_decode(
+        json: $file,
+        associative: true,
+    );
 }
